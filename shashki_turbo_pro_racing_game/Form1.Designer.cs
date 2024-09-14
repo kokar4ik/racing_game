9f7e8cbd2006dd19f797b24b69e36ca6618a82ec
kokar4ik <ilioll2003@gmail.com>
2024-09-14 21:43:43 +0400

    Добавьте файлы проекта.

---
+++ shashki_turbo_pro_racing_game/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace shashki_turbo_pro_racing_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.road = new System.Windows.Forms.PictureBox();
+            this.car = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.roadd = new System.Windows.Forms.PictureBox();
+            this.car2 = new System.Windows.Forms.PictureBox();
+            this.car3 = new System.Windows.Forms.PictureBox();
+            this.car4 = new System.Windows.Forms.PictureBox();
+            this.car5 = new System.Windows.Forms.PictureBox();
+            this.kaboom = new System.Windows.Forms.PictureBox();
+            this.defcars = new System.Windows.Forms.Label();
+            this.btnretry = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kaboom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // road
+            // 
+            this.road.BackColor = System.Drawing.Color.Transparent;
+            this.road.Image = ((System.Drawing.Image)(resources.GetObject("road.Image")));
+            this.road.Location = new System.Drawing.Point(0, 0);
+            this.road.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.road.Name = "road";
+            this.road.Size = new System.Drawing.Size(1000, 1000);
+            this.road.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.road.TabIndex = 0;
+            this.road.TabStop = false;
+            // 
+            // car
+            // 
+            this.car.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.car.Image = ((System.Drawing.Image)(resources.GetObject("car.Image")));
+            this.car.Location = new System.Drawing.Point(610, 681);
+            this.car.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.car.Name = "car";
+            this.car.Size = new System.Drawing.Size(120, 230);
+            this.car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car.TabIndex = 1;
+            this.car.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // roadd
+            // 
+            this.roadd.Image = ((System.Drawing.Image)(resources.GetObject("roadd.Image")));
+            this.roadd.Location = new System.Drawing.Point(0, -990);
+            this.roadd.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.roadd.Name = "roadd";
+            this.roadd.Size = new System.Drawing.Size(1000, 1000);
+            this.roadd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.roadd.TabIndex = 2;
+            this.roadd.TabStop = false;
+            // 
+            // car2
+            // 
+            this.car2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.car2.Image = ((System.Drawing.Image)(resources.GetObject("car2.Image")));
+            this.car2.Location = new System.Drawing.Point(48, -893);
+            this.car2.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.car2.Name = "car2";
+            this.car2.Size = new System.Drawing.Size(120, 230);
+            this.car2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car2.TabIndex = 3;
+            this.car2.TabStop = false;
+            // 
+            // car3
+            // 
+            this.car3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.car3.Image = ((System.Drawing.Image)(resources.GetObject("car3.Image")));
+            this.car3.Location = new System.Drawing.Point(266, -739);
+            this.car3.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.car3.Name = "car3";
+            this.car3.Size = new System.Drawing.Size(130, 270);
+            this.car3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car3.TabIndex = 4;
+            this.car3.TabStop = false;
+            // 
+            // car4
+            // 
+            this.car4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.car4.Image = ((System.Drawing.Image)(resources.GetObject("car4.Image")));
+            this.car4.Location = new System.Drawing.Point(555, -750);
+            this.car4.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.car4.Name = "car4";
+            this.car4.Size = new System.Drawing.Size(120, 230);
+            this.car4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car4.TabIndex = 5;
+            this.car4.TabStop = false;
+            // 
+            // car5
+            // 
+            this.car5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.car5.Image = ((System.Drawing.Image)(resources.GetObject("car5.Image")));
+            this.car5.Location = new System.Drawing.Point(755, -839);
+            this.car5.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.car5.Name = "car5";
+            this.car5.Size = new System.Drawing.Size(120, 230);
+            this.car5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.car5.TabIndex = 6;
+            this.car5.TabStop = false;
+            // 
+            // kaboom
+            // 
+            this.kaboom.BackColor = System.Drawing.Color.NavajoWhite;
+            this.kaboom.Image = ((System.Drawing.Image)(resources.GetObject("kaboom.Image")));
+            this.kaboom.Location = new System.Drawing.Point(74, 36);
+            this.kaboom.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.kaboom.Name = "kaboom";
+            this.kaboom.Size = new System.Drawing.Size(86, 70);
+            this.kaboom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kaboom.TabIndex = 7;
+            this.kaboom.TabStop = false;
+            // 
+            // defcars
+            // 
+            this.defcars.AutoSize = true;
+            this.defcars.Location = new System.Drawing.Point(12, 956);
+            this.defcars.Name = "defcars";
+            this.defcars.Size = new System.Drawing.Size(33, 35);
+            this.defcars.TabIndex = 8;
+            this.defcars.Text = "0";
+            // 
+            // btnretry
+            // 
+            this.btnretry.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnretry.Location = new System.Drawing.Point(430, 123);
+            this.btnretry.Name = "btnretry";
+            this.btnretry.Size = new System.Drawing.Size(153, 51);
+            this.btnretry.TabIndex = 9;
+            this.btnretry.Text = "retry";
+            this.btnretry.UseVisualStyleBackColor = true;
+            this.btnretry.Click += new System.EventHandler(this.btnretry_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(316, 411);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 119);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "ДТП!!!";
+   
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 35F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1000, 1000);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnretry);
+            this.Controls.Add(this.kaboom);
+            this.Controls.Add(this.defcars);
+            this.Controls.Add(this.car);
+            this.Controls.Add(this.car5);
+            this.Controls.Add(this.car4);
+            this.Controls.Add(this.car3);
+            this.Controls.Add(this.car2);
+            this.Controls.Add(this.road);
+            this.Controls.Add(this.roadd);
+            this.Font = new System.Drawing.Font("Showcard Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.road)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.car5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kaboom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox road;
+        private System.Windows.Forms.PictureBox car;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox roadd;
+        private System.Windows.Forms.PictureBox car2;
+        private System.Windows.Forms.PictureBox car3;
+        private System.Windows.Forms.PictureBox car4;
+        private System.Windows.Forms.PictureBox car5;
+        private System.Windows.Forms.PictureBox kaboom;
+        private System.Windows.Forms.Label defcars;
+        private System.Windows.Forms.Button btnretry;
+        private System.Windows.Forms.Label label1;
+  
+    }
+}
+
---
+++ shashki_turbo_pro_racing_game/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shashki_turbo_pro_racing_game
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        private void increasescore ()
+        {
+            score++;
+            defcars.Text = score.ToString();
+        }
+        public Form1()
+
+        {
+            SoundPlayer sound_main = new SoundPlayer(@"C:\Users\iliol\source\repos\shashki_pro_racing_game\shashki_turbo_pro_racing_game\music.mp3.wav");
+            sound_main.PlayLooping();
+            InitializeComponent();
+            kaboom.Visible = false;
+            btnretry.Visible = false;
+            label1.Visible = false;
+            KeyPreview = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                this.Close();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (kaboom.Visible == true)
+            {
+                return;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                if (car.Left > 0)
+                    car.Left += -7;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (car.Right < 1000)
+                    car.Left += 7; 
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (car.Top > 0)
+                    car.Top += -5;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (car.Top < 790)
+                    car.Top += 5;
+            }
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int speed = 15;
+            road.Top += speed;
+            roadd.Top += speed;
+            
+        
+            if (road.Top >= 1000)
+            {
+                road.Top = 0;
+              roadd.Top = -990;
+            }
+            int speed2 = 9;
+            car2.Top += speed2;
+            if (car2.Top>1100)
+            {
+                car2.Top = -330;
+                Random rand = new Random();
+                car2.Left = rand.Next(10, 140);
+                increasescore();
+            }
+            int speed3 = 7;
+            car3.Top += speed3;
+            if (car3.Top > 1100)
+            {
+                car3.Top = -500;
+                Random rand = new Random();
+                car3.Left = rand.Next(250, 395);
+                increasescore();
+            }
+
+            int speed4 = 5;
+            car4.Top += speed4;
+            if (car4.Top > 1100)
+            {
+                car4.Top = -400;
+                Random rand = new Random();
+                car4.Left = rand.Next(505, 640);
+                increasescore();
+            }
+
+            int speed5 = 3;
+            car5.Top += speed5;
+            if (car5.Top > 1100)
+            {
+                car5.Top = -300;
+                Random rand = new Random();
+                car5.Left = rand.Next(750, 863);
+                this.increasescore();
+            }
+            if (car.Bounds.IntersectsWith(car2.Bounds)||
+                car.Bounds.IntersectsWith(car3.Bounds) ||
+                car.Bounds.IntersectsWith(car4.Bounds) ||
+                car.Bounds.IntersectsWith(car5.Bounds))
+            {
+                timer1.Enabled = false;
+                kaboom.Location = this.car.Location;
+                kaboom.Visible = true;
+                label1.Visible = true;
+                btnretry.Visible = true; 
+            }
+        }
+
+      
+
+        private void btnretry_Click_1(object sender, EventArgs e)
+        {
+            car2.Top = -1030;
+            car3.Top = -500;
+            car4.Top = -1400;
+            car5.Top = -900;
+            label1.Visible = false;
+            kaboom.Visible = false;
+            btnretry.Visible = false;
+            timer1.Enabled = true;
+            score = 0;
+            defcars.Text = score.ToString();
+        }
+
+   
+
+    
+    }
+}
